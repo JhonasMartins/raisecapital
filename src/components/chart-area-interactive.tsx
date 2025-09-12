352c8cb6c6fad3b1f3477ace05e0cbe46b747abb
I2H <94246458+JhonasMartins@users.noreply.github.com>
2025-09-12 10:52:39 -0300

    refactor(components): rename CardAction to CardToolbar for clarity

--- src/components/chart-area-interactive.tsx
+++ src/components/chart-area-interactive.tsx
@@ -6,7 +6,7 @@ import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 import { useIsMobile } from "@/hooks/use-mobile"
 import {
   Card,
-  CardAction,
+  CardToolbar,
   CardContent,
   CardDescription,
   CardHeader,
@@ -174,7 +174,7 @@ export function ChartAreaInteractive() {
           </span>
           <span className="@[540px]/card:hidden">Last 3 months</span>
         </CardDescription>
-        <CardAction>
+        <CardToolbar>
           <ToggleGroup
             type="single"
             value={timeRange}
@@ -206,7 +206,7 @@ export function ChartAreaInteractive() {
               </SelectItem>
             </SelectContent>
           </Select>
-        </CardAction>
+        </CardToolbar>
       </CardHeader>
       <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
         <ChartContainer
